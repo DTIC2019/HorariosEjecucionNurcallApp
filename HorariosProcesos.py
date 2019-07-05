de74f6233b91dc248e4441ae561d2557e28b1f2a
DTIC2019 <52576852+DTIC2019@users.noreply.github.com>
2019-07-05 10:36:18 -0500

    Add files via upload

---
+++ HorariosProcesos.py
@@ -0,0 +1,42 @@
+#Tener presente que los horarios no se crucen
+#por ejemplo, no ejecutar un proceso mientras haya un reinicio
+
+
+
+horasReinicio = [(12, 30),
+                 (23, 30)]
+
+horasEnviarFoto = [(16, 0)]
+
+horasBorrarFoto = [(16, 10)] #Es necesario para que la metrica en la grafica muestre los datos en las ultimas 24 horas
+
+horasEnviarTelegram = [(6, 0),
+                      (10, 0),
+                      (14, 0),
+                      (18, 0),
+                      (22, 0)]
+
+horasEjecutarRobot = [(1, 0),
+                      (2, 0),
+                      (3, 0),
+                      (4, 0),
+                      (5, 0),
+                      (6, 0),
+                      (7, 0),
+                      (8, 0),
+                      (9, 0),
+                      (10, 0),
+                      (11, 0),
+                      (12, 0),
+                      (13, 0),
+                      (14, 0),
+                      (15, 0),
+                      (16, 0),
+                      (17, 0),
+                      (18, 0),
+                      (19, 0),
+                      (20, 0),
+                      (21, 0),
+                      (22, 0),
+                      (23, 0),
+                      (23, 50)]
